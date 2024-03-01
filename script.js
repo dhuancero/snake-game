15cb10023f38b5cbb3b8f65994178cb93d692b6e
David Huanco Acero <dha.desarrollos@gmail.com>
2024-03-01 22:47:29 +0100

    Añadido JS

---
+++ script.js
@@ -0,0 +1,6 @@
+//? HTML Elementos
+
+const board = document.getElementById("board");
+const scoreBoard = document.getElementById("scoreBoard");
+const startBtn = document.getElementById("start");
+const gameOverSign = document.getElementById("gameOver");
