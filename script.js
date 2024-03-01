baba8b4e1c9e1ed621c022677f01abf13f294af6
David Huanco Acero <dha.desarrollos@gmail.com>
2024-03-01 23:32:30 +0100

    Segundo Commit

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 //? HTML Elementos
 
+//
 const board = document.getElementById("board");
 const scoreBoard = document.getElementById("scoreBoard");
-const startBtn = document.getElementById("start");
+const startButton = document.getElementById("start");
 const gameOverSign = document.getElementById("gameOver");
+
+// Game settings
+const boardSize = 10;
+const gameSpeed = 100;
+const squareTypes = {
+  emptySquare: 0,
+  snakeSquare: 1,
+  foodSquare: 2,
+};
+
+// Mapeamos direcciones
+const directions = {
+  ArrowUp: -10,
+  ArrowDown: 10,
+  ArrowRight: 1,
+  ArrowLeft: -1,
+};
+
+// Variable del juego
+
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const createBoard = () => {};
+
+const setGame = () => {
+  snake = ["00", "01", "02", "03"];
+  score = snake.length;
+  direction = "ArrowRight";
+  boardSquares = Array.from(Array(boardSize), () =>
+    new Array(boardSize).fill(squareTypes.emptySquare)
+  );
+  console.log(boardSquares);
+  board.innerHTML = "";
+  emptySquares = [];
+  createBoard();
+};
+
+const startGame = () => {
+  setGame();
+};
+
+startButton.addEventListener("click", startGame);
